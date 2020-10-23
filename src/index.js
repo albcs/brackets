a5421928c39125e1d4eef42e49b3e61b52e5ddc3
Albert Nurkaev <albert.nurkaev.cs@gmail.com>
2020-10-23 17:57:53 +0200

    feat: implement the brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // your solution
+    let arrOneDmn = [];
+
+    bracketsConfig.forEach((element) => {
+        arrOneDmn.push(element.join(""));
+    });
+
+    let counter = str.length;
+    for (let i = 0; i < counter; i++) {
+        arrOneDmn.forEach((element) => {
+            str = str.replace(element, "");
+        });
+    }
+
+    if (str.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
